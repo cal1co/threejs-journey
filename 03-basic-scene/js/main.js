9cdd36c8cb6abee08d91796c9399f773ba5366ee
Alex King <calicoalix@gmail.com>
2022-03-24 00:14:29 +1100

    finished lesson 1

--- 03-basic-scene/js/main.js
+++ 03-basic-scene/js/main.js
@@ -2,13 +2,35 @@ console.log('hello THREE');
 console.log(THREE);
 // const THREE = require('./three.min.js')
 
+const canvas = document.querySelector('.webgl')
+console.log(canvas)
+
+// Scene
 const scene = new THREE.Scene()
 
-
+// Mesh
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color:0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+scene.add( mesh )
 
-const camera = new THREE.PerspectiveCamera(75, )
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
 scene.add(camera)
+
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas:canvas
+})
+
+renderer.setSize(sizes.width, sizes.height)
+
+renderer.render(scene, camera)
