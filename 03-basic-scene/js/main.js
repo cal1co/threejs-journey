d0dc18d08dcb7e23cd54b4bb3264f5159dbd868e
Alex King <calicoalix@gmail.com>
2022-03-24 00:15:50 +1100

    changes to pos

--- 03-basic-scene/js/main.js
+++ 03-basic-scene/js/main.js
@@ -23,6 +23,8 @@ const sizes = {
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+camera.position.x = 1
+camera.position.y = 1
 scene.add(camera)
 
 
