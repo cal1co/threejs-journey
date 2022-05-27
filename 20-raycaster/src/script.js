10233bfc8e73ee10c727de9564698d393e5125c7
Alex King <calicoalix@gmail.com>
2022-05-27 15:53:29 +1000

    20 complete

--- 20-raycaster/src/script.js
+++ 20-raycaster/src/script.js
@@ -38,6 +38,23 @@ object3.position.x = 2
 scene.add(object1, object2, object3)
 
 /**
+ * Raycaster
+ */
+const raycaster = new THREE.Raycaster()
+// const rayOrigin = new THREE.Vector3(-3, 0, 0)
+// const rayDirection = new THREE.Vector3(10, 0, 0)
+// rayDirection.normalize()
+// raycaster.set(rayOrigin, rayDirection)
+
+// const intersect = raycaster.intersectObject(object2)
+// console.log(intersect);
+
+// const intersects = raycaster.intersectObjects([object1, object2, object3])
+// console.log(intersects);
+
+
+
+/**
  * Sizes
  */
 const sizes = {
@@ -59,6 +76,19 @@ window.addEventListener('resize', () =>
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
+
+/**
+ * Mouse
+ */
+const mouse = new THREE.Vector2()
+
+window.addEventListener('mousemove', (e) => {
+    // console.log('mouse move');
+    mouse.x = e.clientX / sizes.width * 2 - 1
+    mouse.y = -(e.clientY / sizes.height) * 2 + 1
+})
+
+
 
 /**
  * Camera
@@ -89,6 +119,43 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Animate Objs
+    object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5
+    object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5
+    object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
+
+    // Cast a ray
+    raycaster.setFromCamera(mouse, camera)
+
+
+    // const rayOrigin = new THREE.Vector3(-3, 0, 0)
+    // const rayDirection = new THREE.Vector3(10, 0, 0)
+    // rayDirection.normalize()
+
+    // raycaster.set(rayOrigin, rayDirection)
+
+    const objectsToTest = [object1, object2, object3]
+    // const intersect = raycaster.intersectObject(object2)
+    const intersects = raycaster.intersectObjects(objectsToTest)
+    // // if (intersect.length > 0){
+    // //     // console.log(intersect);
+    // // }
+    // // if (intersects.length === 3){
+    // //     // console.log(intersects);
+    // // }
+
+    for(const object of objectsToTest){
+        object.material.color.set('#ff0000')
+    }
+
+    for (const intersect of intersects) {
+        // console.log(intersect.object);
+        intersect.object.material.color.set('#0000ff')
+    }
+
+
+
 
     // Update controls
     controls.update()
